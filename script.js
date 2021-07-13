53a816b970cb0ae580ae1ea69a76bf3c62c5c8a0
laura <ll.calderon14@gmail.com>
2021-07-13 17:24:30 -0500

    Realizacion matriz con filas y columnas

---
+++ script.js
@@ -0,0 +1,24 @@
+
+ 
+ function matriz(v1,v2){
+     let dibujar=document.getElementById("container1")
+     dibujar.style.width=parseInt(72*v2)+"px"
+     dibujar.style.height=parseInt(72*v1)+"px"
+     
+     console.log(parseInt((70*v2)+parseInt(v2)*1.5)+"px")
+     let cuadro=""
+     for(i=0;i<v1;i++){
+         for(p=0;p<v2;p++){
+          cuadro= cuadro + `<div></div>`;
+         }
+     }
+     dibujar.innerHTML=cuadro
+     return cuadro
+ }
+ let filas =prompt("Digite la cantidad de filas que quiere dibujar ")
+ let columnas=prompt("Digite la cantidad de columnas que quiere dibujar")
+ 
+ let resultado= filas*columnas;
+ 
+ matriz(filas,columnas);
+ 
